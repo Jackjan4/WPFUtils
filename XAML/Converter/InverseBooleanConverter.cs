c8d480846178ee6b57d5145266cdf7d435be1463
Jan <Jackjan@protonmail.com>
2019-06-27 12:55:48 +0200

    Added InverseBooleanConverter

---
+++ XAML/Converter/InverseBooleanConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace De.JanRoslan.WPFUtils.XAML.Converter {
+
+    [ValueConversion(typeof(bool), (typeof(int)))]
+    public class InverseBooleanConverter : IValueConverter {
+
+
+
+        /// <summary>
+        /// Boolean to inverse boolean
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is bool b) {
+                return !b;
+            }
+
+            return false;
+        }
+
+
+
+        /// <summary>
+        /// Boolean to inverse boolean
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is bool b) {
+                return !b;
+            }
+
+            return false;
+        }
+    }
+}
